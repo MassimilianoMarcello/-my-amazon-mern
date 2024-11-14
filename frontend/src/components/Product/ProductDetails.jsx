c2e246c41e56e2403abc868d4ce030ee1d9eca69
massimiliano <massimarcello@hotmail.it>
2024-11-14 17:59:57 +0100

    products done

--- frontend/src/components/Product/ProductDetails.jsx
+++ frontend/src/components/Product/ProductDetails.jsx
@@ -1,5 +1,6 @@
-import  { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import './ProductDetails.css';
 
 const ProductDetails = () => {
@@ -13,13 +14,9 @@ const ProductDetails = () => {
     useEffect(() => {
         const fetchProduct = async () => {
             try {
-                const response = await fetch(`http://localhost:5004/api/products/${id}`);
-                if (!response.ok) {
-                    throw new Error('Failed to fetch product');
-                }
-                const data = await response.json();
-                setProduct(data);
-                setMainImage(data.mainImage); // set main Image
+                const response = await axios.get(`http://localhost:5004/api/products/${id}`);
+                setProduct(response.data);
+                setMainImage(response.data.mainImage); // Imposta l'immagine principale
             } catch (error) {
                 setError(error.message);
             } finally {
@@ -40,7 +37,6 @@ const ProductDetails = () => {
 
     return (
         <div className="product-details">
-            <img src={mainImage} alt={product.title} className="product-main-image" />
             <div className="product-thumbnails">
                 <img
                     src={product.mainImage}
@@ -58,16 +54,19 @@ const ProductDetails = () => {
                     />
                 ))}
             </div>
+            <div>
+                <img src={mainImage} alt={product.title} className="product-main-image" />
+                <div className="product-buttons">
+                    <button className="add-to-cart-button">Add to Cart</button>
+                    <button className="edit-product-button" onClick={() => navigate(`/update-product/${id}`)}>Edit</button>
+                    <button className="delete-product-button" onClick={() => navigate(`/delete-product/${id}`)}>Delete</button>
+                </div>
+            </div>
             <div className="product-info">
                 <h2 className="product-title">{product.title}</h2>
                 <p className="product-description">{product.description}</p>
                 <p className="product-category">Category: {product.category}</p>
                 <p className="product-price">${product.price.toFixed(2)}</p>
-                <button className="add-to-cart-button">Add to Cart</button>
-                <button className="edit-product-button" onClick={() => navigate(`/update-product/${id}`)}>Edit</button>
-    
-                <button className="delete-product-button" onClick={() => navigate(`/delete-product/${id}`)}>Delete</button>
-
             </div>
         </div>
     );
